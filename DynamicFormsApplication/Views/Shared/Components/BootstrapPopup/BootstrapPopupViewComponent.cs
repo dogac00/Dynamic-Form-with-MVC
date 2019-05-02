9cbe7747ba0fff356eaa235d157a417465d8cb1b
Dogac Akyildiz <ndakyildiz@gmail.com>
2019-05-02 22:51:50 +0300

    ViewComponent Added.

---
+++ DynamicFormsApplication/Views/Shared/Components/BootstrapPopup/BootstrapPopupViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using DynamicFormsApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicFormsApplication.Views.Shared.Components.BootstrapPopup
+{
+    public class BootstrapPopupViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BootstrapPopupViewComponent(ApplicationDbContext dbContext)
+        {
+            this._context = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var forms = await _context.Forms.ToListAsync();
+            return View(forms);
+        }
+    }
+}
